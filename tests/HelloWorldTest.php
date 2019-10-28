8e323f62eb4fa41e3c50a549e4671d96edb50153
Peter Gribanov <wcode404@gmail.com>
2019-10-28 15:41:48 +0300

    Create HelloWorldTest.php

---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use StyleCI\HelloWorld;
+use PHPUnit\Framework\TestCase;
+
+class HelloWorldTest extends TestCase
+{
+    private $hw;
+
+    protected function setUp(): void
+    {
+        $this->hw = new HelloWorld();
+    }
+
+    public function testAlreadyStarted(): void
+    {
+        $this->hw->sayHello(new \DateTimeImutable());
+    }
+}
