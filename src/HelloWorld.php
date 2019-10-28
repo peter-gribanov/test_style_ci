b596f4031a4c4b1925439927f8659a8312167af7
Peter Gribanov <wcode404@gmail.com>
2019-10-28 15:39:47 +0300

    Update HelloWorld.php

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
 
+namespace StyleCI;
+
 class HelloWorld
 {
-	public function sayHello(DateTimeImutable $date): void
+	public function sayHello(\DateTimeImutable $date): void
 	{
 		echo 'Hello, ' . $date->format('j. n. Y');
 	}
